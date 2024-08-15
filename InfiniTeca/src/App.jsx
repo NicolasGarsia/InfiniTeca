522c8fa7569fa313859f44160806afdbc37de98c
NICOLAS SCHNEIDER LAPOLLI GARCIA <nicolas_s_garcia@estudante.sesisenai.org.br>
2024-08-15 08:52:34 -0300

    Arquivo Base

---
+++ InfiniTeca/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Home from './pages/Home'
+import Perfil from './pages/Perfil'
+
+function App() {
+  
+
+  return (
+    <>
+      <Home />
+      <Perfil />
+    </>
+  )
+}
+
+export default App
---
+++ InfiniTeca/src/components/NavB.jsx
@@ -0,0 +1,9 @@
+export default function NavB(){
+    return(
+
+        <div>
+            AAAA
+        </div>
+
+    )
+}
