4ca377fb265ec27c5cc803d8e45ebc249214ad24
ANA LUIZA DE CASTRO <ana_l_castro@estudante.sesisenai.org.br>
2024-09-12 07:58:00 -0300

    vtmnc

--- InfiniTeca1/src/components/home1/Livros.jsx
+++ InfiniTeca1/src/components/home1/Livros.jsx
@@ -9,48 +9,48 @@ export default function Liv() {
     return (
       <>
       <Carousel/>
-        <h2 id="txt"> Recomendados </h2>
+      <h2 id="txt"> Recomendados </h2>
   
-        <div id="estante" className="estant">
-          <div className="Book" onClick={() => navigate("/Livro")}>
-            <img className="livroh" src="harry.png" alt="Nome Livro" />
-            <p>Harry Potter</p>
-            <p>Disponiveis (02/05)</p>
-          </div>
-          <div className="Book" onClick={() => navigate("/Livro")}>
-            <img className="livroh" src="Livro.jpg" alt="Nome Livro" />
-            <p>O Hobbit</p>
-            <p>Disponiveis (03/05)</p>
-          </div>
-          <div className="Book" onClick={() => navigate("/Livro")}>
-            <img className="livroh" src="Livro.jpg" alt="Nome Livro" />
-            <p>A garota do trem</p>
-            <p>Disponiveis (01/05)</p>
-          </div>
-          <div className="Book" onClick={() => navigate("/Livro")}>
-            <img className="livroh" src="Livro.jpg" alt="Nome Livro" />
-            <p>O sol é para todos</p>
-            <p>Disponiveis (05/05)</p>
-          </div>
-          <div className="Book" onClick={() => navigate("/Livro")}>
-            <img className="livroh" src="Livro.jpg" alt="Nome Livro" />
-            <p>Dom Casmurro</p>
-            <p>Disponiveis (01/05)</p>
-          </div>
-          <div className="Book" onClick={() => navigate("/Livro")}>
-            <img className="livroh" src="Livro.jpg" alt="Nome Livro" />
-            <p>1984</p>
-            <p>Disponiveis (00/05)</p>
-          </div><div className="Book" onClick={() => navigate("/Livro")}>
-            <img className="livroh" src="Livro.jpg" alt="Nome Livro" />
-            <p>Cem Anos de Solidão</p>
-            <p>Disponiveis (04/05)</p>
-          </div><div className="Book" onClick={() => navigate("/Livro")}>
-            <img className="livroh" src="Livro.jpg" alt="Nome Livro" />
-            <p>O menino do pijama listrado</p>
-            <p>Disponiveis (01/05)</p>
-          </div>
-        </div>
+  <div id="estante" className="estant">
+    <div className="Book" onClick={() => navigate("/Livro")}>
+      <img className="livroh" src="harry.png" alt="Nome Livro" />
+      <p>Harry Potter</p>
+      <p>Disponiveis (02/05)</p>
+    </div>
+    <div className="Book" onClick={() => navigate("/Livro")}>
+      <img className="livroh" src="hobbit.png" alt="Nome Livro" />
+      <p>O Hobbit</p>
+      <p>Disponiveis (03/05)</p>
+    </div>
+    <div className="Book" onClick={() => navigate("/Livro")}>
+      <img className="livroh" src="garota.png" alt="Nome Livro" />
+      <p>A garota do trem</p>
+      <p>Disponiveis (01/05)</p>
+    </div>
+    <div className="Book" onClick={() => navigate("/Livro")}>
+      <img className="livroh" src="sol.png" alt="Nome Livro" />
+      <p>O sol é para todos</p>
+      <p>Disponiveis (05/05)</p>
+    </div>
+    <div className="Book" onClick={() => navigate("/Livro")}>
+      <img className="livroh" src="casmurro.png" alt="Nome Livro" />
+      <p>Dom Casmurro</p>
+      <p>Disponiveis (01/05)</p>
+    </div>
+    <div className="Book" onClick={() => navigate("/Livro")}>
+      <img className="livroh" src="1984.png" alt="Nome Livro" />
+      <p>1984</p>
+      <p>Disponiveis (0/05)</p>
+    </div><div className="Book" onClick={() => navigate("/Livro")}>
+      <img className="livroh" src="cem.png" alt="Nome Livro" />
+      <p>Cem Anos de Solidão</p>
+      <p>Disponiveis (04/05)</p>
+    </div><div className="Book" onClick={() => navigate("/Livro")}>
+      <img className="livroh" src="menino.png" alt="Nome Livro" />
+      <p>O menino do pijama listrado</p>
+      <p>Disponiveis (01/05)</p>
+    </div>
+  </div>
 
       </>
     );
